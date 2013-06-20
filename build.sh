285e7af7609928b35934e47109bafbf7ada78cf6
Yiling Cao <yiling.cao@gmail.com>
2013-06-20 17:55:26 +0800

    tweak gcc compiler options.

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 CROSS_CC=arm-linux-gnueabi-gcc
 
-gcc -o arm_c_benchmark benchmark.c
+gcc -O3 arm_c_benchmark benchmark.c
+gcc -O3 -cpu=Cortex-A8 -mfpu=neon -o arm_c_benchmark_neon benchmark.c
+
 #gcc -O3 -o arm_c_benchmark_O3 benchmark.c
 #gcc -O2 -o arm_c_benchmark_O2 benchmark.c
 
 #cross complie
 
-$CROSS_CC -static -march=armv7 -o arm_c_benchmark_armv7 benchmark.c
-$CROSS_CC -static -o arm_c_benchmark_arm_generic benchmark.c
+#$CROSS_CC -static -march=armv7 -o arm_c_benchmark_armv7 benchmark.c
+#$CROSS_CC -static -o arm_c_benchmark_arm_generic benchmark.c
 
